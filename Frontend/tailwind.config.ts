a8371618ec0e74212162fba11f99f1be50c98267
Kazbek <37765@iitu.edu.kz>
2025-10-05 21:53:27 +0500

    Fixed bugs with styles

--- Frontend/tailwind.config.ts
+++ Frontend/tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
     // Screen
